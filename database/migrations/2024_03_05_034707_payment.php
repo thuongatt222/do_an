6abfb3ce85eb62c97a4573f9ba7fc7336b9efcb2
thuongatt222 <thuongatt2k3@gmail.com>
2024-03-07 11:04:54 +0700

    migration

--- database/migrations/2024_03_05_034707_payment.php
+++ database/migrations/2024_03_05_034707_payment.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('payment_method', function (Blueprint $table) {
             $table->id('payment_method_id');
             $table->string('payment_method');
+            $table->date('payment_date');
+            $table->string('payment_status');
             $table->timestamps();
         });
     }
