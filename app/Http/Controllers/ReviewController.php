68a0745044f5d70cdb37dea7c48d4bb4764a9b41
thuongatt222 <thuongatt2k3@gmail.com>
2024-03-13 08:28:39 +0700

    review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,24 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Review\StoreReviewRequest;
+use App\Http\Requests\Review\UpdateReviewRequest;
+use App\Http\Resources\Review\ReviewResource;
+use App\Models\Review;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
+    protected $review;
+    public function __construct(Review $review)
+    {
+        $this->review = $review;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $review = $this->review->paginate(5);
+        $reviewResource = ReviewResource::collection($review)->response()->getData(true);
+        return response()->json([
+            'data' => $reviewResource,
+        ], HttpResponse::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreReviewRequest $request)
     {
-        //
+        $dataCreate = $request->all();
+        $review = $this->review->create($dataCreate);
+        $reviewResource = new ReviewResource($review);
+        return response()->json([
+            'data' => $reviewResource,
+        ], HttpResponse::HTTP_OK);
     }
 
     /**
@@ -33,16 +52,26 @@ class ReviewController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateReviewRequest $request, string $id)
     {
-        //
+        $review = $this->review->findOrFail($id);
+        $dataUpdate = $request->all();
+        $review->update($dataUpdate);
+        $reviewResource = new ReviewResource($review);
+        return response()->json([
+            'data' => $reviewResource,
+        ], HttpResponse::HTTP_OK);
     }
-
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $review = $this->review->where('review_id', $id)->firstOrFail();
+        $review->delete();
+        $reviewResource = new ReviewResource($review);
+        return response()->json([
+            'data' => $reviewResource,
+        ], HttpResponse::HTTP_OK);
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'review',
+        'evaluation',
+    ];
+    protected $primaryKey = 'review_id';
+    protected $table = 'review_and_evaluation';
 }
